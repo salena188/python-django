f6c0cf48ec73f7016c9d439e316d11331228a8ce
salena188 <119055604+salena188@users.noreply.github.com>
2026-05-29 00:20:12 +0545

    Delete python_django/pythononeliner directory

--- python_django/pythononeliner/list_comprehension.py
+++
@@ -1,9 +0,0 @@
-print("\n------List Comprehension----")
-squares = []
-for i in range(1,6):
-   squares.append(i*i)
-print(squares)
-
-print("------List Comprehension One-Liner----")
-squares = [i*i for i in range(1,6)]
-print(squares)
--- python_django/pythononeliner/read_file.py
+++
@@ -1,3 +0,0 @@
-print("\n------Read File One-Liner----")
-data = open("file.txt").read()
-print(data)
